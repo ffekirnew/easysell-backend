45b88e6e7b356cddf261030d5435bdc79048a370
ffekirnew <ffekirnew0808@gmail.com>
2023-09-24 11:37:15 +0300

    Finish setting up application

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from src.webapi.controllers import product_controller, user_controller
+
+app = FastAPI()
+app.include_router(user_controller.user_router)
+app.include_router(product_controller.product_router)
